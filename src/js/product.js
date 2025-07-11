20b06e25222b36677281899f1a6b5aef8b6bdc38
Cristhina <viszellechacon@gmail.com>
2025-07-11 17:31:52 -0500

    changing single quotes per double quotes

--- src/js/product.js
+++ src/js/product.js
@@ -3,8 +3,8 @@ import ProductData from "./ProductData.mjs";
 import productDetails from "./productDetails.mjs";
 
 
-const productId = getParam('product');
-const dataSource = new ProductData('tents');
+const productId = getParam("product");
+const dataSource = new ProductData("tents");
 
 const product = new productDetails(productId, dataSource);
 product.init();
