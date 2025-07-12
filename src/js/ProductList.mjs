a1bf2e27d9aafc9613ddbcf6695f7e08e2afb54d
Cristhina <viszellechacon@gmail.com>
2025-07-12 17:03:43 -0500

    Discount indicator-product listing Done

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -19,12 +19,28 @@ export default class ProductList {
 }
 
 function productCardTemplate(product) {
+  const isOnSale = product.FinalPrice < product.ListPrice;
+  const discount = isOnSale
+    ? Math.round(
+        ((product.SuggestedRetailPrice - product.FinalPrice) /
+          product.SuggestedRetailPrice) *
+          100
+      )
+    : 0;
+
   return `<li class="product-card">
     <a href="product_pages/?product=${product.ID}">
       <img src="${product.Image}" alt="${product.Name}">
       <h2 class="card__brand">${product.Brand.Name}</h2>
       <h3 class="card__name">${product.Name}</h3>
-      <p class="product-card__price">$${product.FinalPrice}</p>
+      <p class="product-card__price">  $${product.FinalPrice}
+        ${isOnSale ? `<span class="original-price">$${product.SuggestedRetailPrice}</span>` : ""}
+      </p>
+      ${
+        isOnSale
+          ? `<span class="discount-badge">${discount}% OFF</span>`
+          : ""
+      }
     </a>
   </li>`;
 }
