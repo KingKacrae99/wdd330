3762c465567c421518e14866e5e954a7b68a78f3
Cristhina <viszellechacon@gmail.com>
2025-08-02 19:57:07 -0500

    update

--- src/js/cart.js
+++ src/js/cart.js
@@ -11,21 +11,23 @@ function renderCartContents() {
 
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  const newItem = `<li class="cart-card divider" data-id="${item.Id}">
+    <a href="#" class="cart-card__image">
+      <img src="${item.Image}" alt="${item.Name}" />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    
+    <div class="cart-card__quantity-controls">
+      <button class="decrease-qty" data-id="${item.Id}">−</button>
+      <span class="cart-card__quantity">qty: ${item.quantity || 1}</span>
+      <button class="increase-qty" data-id="${item.Id}">+</button>
+    </div>
 
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
   return newItem;
 }
 
