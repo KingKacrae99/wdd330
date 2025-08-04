af2690ceb3804e7acba4b56dd8207f26f2b0fcaf
Cristhina <viszellechacon@gmail.com>
2025-08-03 21:27:09 -0500

    individual finish

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,4 +1,5 @@
 import CheckoutProcess from "./checkoutProcess.mjs";
+import { alertMessage } from "./utils.mjs";
 
 const checkout = new CheckoutProcess("so-cart", ".order-summary");
 checkout.init();
@@ -9,16 +10,30 @@ document.querySelector("#checkoutSubmit").addEventListener("click", async (e) =>
   const myForm = document.forms[0]; 
   const isValid = myForm.checkValidity();
   myForm.reportValidity();
+  alertMessage()
 
-  if (!isValid) return; 
+  if (!isValid) {
+    const invalidFields = [...myForm.elements].filter(field => !field.validity.valid);
+    
+
+    const messages = invalidFields.map(field => {
+      const label = myForm.querySelector(`label[for="${field.id}"]`);
+      const name = label ? label.textContent : field.name || field.id || "Unknown field";
+      return `• ${name} is invalid.`;
+    }).join("\n");
+
+    alertMessage("Please fix the following:\n\n" + messages);
+    return;
+  } 
 
   try {
     await checkout.checkout(e.target);
     alert("Order successful!");
     localStorage.removeItem("so-cart");
+    alertMessage("Order successful!");
     window.location.href = "../checkout/success.html";
   } catch (err) {
     alert("Order failed. Try again.");
-    console.error(err);
+    alertMessage("Order failed. Try again.");
   }
 });
